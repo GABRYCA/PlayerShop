67a2a8d86e5d7dd13a7ed8f8335f82acbb8fe4f2
GABRYCA <gabrycaretti@gmail.com>
2019-08-04 12:10:04 +0200

    Version 1.2
    Changelogs:
    
    - Now if you try to open a GUI while no shops got ever created, a message will be send to the player and the GUI won't open, this is to avoid an ERROR.

--- src/it/gabryca/playershop/shops.java
+++ src/it/gabryca/playershop/shops.java
@@ -3,6 +3,7 @@ package it.gabryca.playershop;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.Configuration;
 import org.bukkit.entity.Player;
 
 public class shops implements CommandExecutor {
@@ -10,11 +11,16 @@ public class shops implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
 
-        if (commandSender instanceof Player) {
+        Configuration config = Main.getInstance().getConfig();
 
-            Player p = (Player) commandSender;
-            GUI gui = new GUI(p);
-            gui.open();
+        if (commandSender instanceof Player) {
+            if (config.getConfigurationSection("shops") != null) {
+                Player p = (Player) commandSender;
+                GUI gui = new GUI(p);
+                gui.open();
+            } else {
+                commandSender.sendMessage("§c" + config.getString("Messages.Warn-NoShopsEverCreated"));
+            }
 
         }
             return true;
